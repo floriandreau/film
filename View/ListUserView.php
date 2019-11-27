fe57c3d2a14be97680095bdb300f9a143bf5b9ed
floriandreau <36734324+floriandreau@users.noreply.github.com>
2019-11-27 11:12:33 +0100

    Admin

--- View/ListUserView.php
+++ View/ListUserView.php
@@ -3,12 +3,77 @@
 $title = "Liste Utilisateurs";
 include 'header.php';
 
+if (isset($_POST['ModifUser'])) {
+    require('env.php');
+
+$dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+
+$ModUser = $dbh->prepare("UPDATE utilisateur SET role_utilisateur= {$_POST['roleUser']} WHERE id_utilisateur={$_POST['idUser']}");
+    $ModUser->execute();
+
+    $ModUser = $ModUser->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Location: index.php?page=ListUser');
+}
+
+if (isset($_POST['SuppUser'])) {
+    require('env.php');
+
+$dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+
+$ModUser = $dbh->prepare("DELETE FROM `utilisateur` WHERE id_utilisateur={$_POST['idSuppUser']}");
+    $ModUser->execute();
+
+    $ModUser = $ModUser->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Location: index.php?page=ListUser');
+}
 
 ?>
 
-<p>
-Liste Utilisateurs
-</p>
+<table>
+<thead>
+    <th>Pseudo</th>
+    <th>Role</th>
+    <th>Option</th>
+</thead>
+<tbody>
+<?php
+    foreach ($all_User as $key => $value) :
+        if ($value['role_utilisateur'] != 2) {
+
+?>
+<tr>
+<td>
+        <?php
+        echo $value['pseudo_utilisateur'];
+        ?>
+        </td>
+        <td>
+        <form method="POST">
+        <input type="hidden" name="idUser" value="<?php echo $value['id_utilisateur'];?>">
+            <select name="roleUser">
+                <option value="<?php echo $value['role_utilisateur'];?>">0</option>
+                <option value="1">1</option>
+            </select>
+            <button name="ModifUser" type="submit">Modifier</button>
+        </form>
+        </td>
+        <td>
+        <form method="POST">
+        <input type="hidden" name="idSuppUser" value="<?php echo $value['id_utilisateur'];?>">
+            <button name="SuppUser" type="submit">Supprimer</button>
+        </form>
+        </td>
+</tr>
+<?php
+    }
+endforeach
+?>
+</tbody>
+</table>
 
 <?php
 
--- View/header.php
+++ View/header.php
@@ -19,6 +19,20 @@ session_start();
     <li><a href="index.php?page=ListFilm">Films</a></li>
     <li><a href="index.php?page=ListReal">Réalisateurs</a></li>
     <li><a href="index.php?page=ListAct">Acteurs</a></li>
+<?php
+
+if (isset($_SESSION['role'])) {
+    if ($_SESSION['role'] == 1 || $_SESSION['role'] == 2) {
+        
+?>
+<li>
+<a href="index.php?page=ListUser">User</a>
+</li>
+<?php
+    }
+}
+
+?>
 </ul>
 </nav>
 <h1><a href="index.php">Logo</a></h1>
@@ -41,22 +55,5 @@ else{
 
 </header>
 
-<!-- <div>
-<form action="inscription.php" method="POST">
-<input name="Pseudo" type="text" placeholder="Pseudo">
-<input name="Pass" type="password" placeholder="Mot de passe">
-<input name="Comfpass" type="password" placeholder="Comfirmation mot de passe">
-<input name="inscription" type="submit">
-</form>
-</div> -->
-
 <?php require('inscription.php') ?>
 <?php require('connection.php') ?>
-
-<!-- <div>
-<form action="connection.php" method="POST">
-<input name="Pseudo" type="text" placeholder="Pseudo">
-<input name="Pass" type="password" placeholder="Mot de passe">
-<input name="Co" type="submit">
-</form>
-</div> -->
