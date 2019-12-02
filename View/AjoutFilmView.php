d215f9b4376f6c95797445afb64e06bc8d8f5cda
floriandreau <36734324+floriandreau@users.noreply.github.com>
2019-12-02 09:09:44 +0100

    fini?

--- View/AjoutFilmView.php
+++ View/AjoutFilmView.php
@@ -15,13 +15,34 @@ include 'header.php';
 <input placeholder="studio" type="text" name="studio">
 <input placeholder="affiche" type="file" name="affiche">
 <input placeholder="URL bande annonce" type="text" name="bande">
-<textarea name="syno" id="" cols="30" rows="10" placeholder="Synopsis"></textarea>
+<textarea name="syno" cols="30" rows="10" placeholder="Synopsis"></textarea>
 
 
 <!-- Créer un nouv choix de genre -->
 <p>Choix Genre</p>
 <div id="ChoixGenre">
-<select name="Genre" id="">
+<select name="Genre" >
+<?php
+foreach ($listG as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_genre']; ?>"><?php echo $valuer['type_genre']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Genre1" >
+<option value="">Aucun</option>
+<?php
+foreach ($listG as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_genre']; ?>"><?php echo $valuer['type_genre']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Genre2" >
+<option value="">Aucun</option>
+
 <?php
 foreach ($listG as $key => $valuer) {
     ?>
@@ -31,33 +52,95 @@ foreach ($listG as $key => $valuer) {
 ?>
 </select>
 </div>
-<button onclick="plusGenre()">Plus</button>
-<button>Ajouter Genre</button>
+<p onclick="popgenre()">Créer Genre</p>
 
 
 <!-- Créer un nouv choix de acteur -->
 
 <p>Choix acteur</p>
 <div id="ChoixAct">
-<select name="Act" id="">
+<select name="Act">
 <?php
-foreach ($listA as $key => $value) {
+foreach ($listA as $key => $valuer) {
     ?>
-    <option value="<?php echo $value['id_acteur']; ?>"><?php echo $value['nom_acteur'].' '.$value['prenom_acteur']; ?></option>
+    <option value="<?php echo $valuer['id_acteur']; ?>"><?php echo $valuer['nom_acteur'].' '.$valuer['prenom_acteur']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Act1">
+<option value="">Aucun</option>
+
+<?php
+foreach ($listA as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_acteur']; ?>"><?php echo $valuer['nom_acteur'].' '.$valuer['prenom_acteur']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Act2">
+<option value="">Aucun</option>
+
+<?php
+foreach ($listA as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_acteur']; ?>"><?php echo $valuer['nom_acteur'].' '.$valuer['prenom_acteur']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Act3">
+<option value="">Aucun</option>
+
+<?php
+foreach ($listA as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_acteur']; ?>"><?php echo $valuer['nom_acteur'].' '.$valuer['prenom_acteur']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Act4">
+<option value="">Aucun</option>
+
+<?php
+foreach ($listA as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_acteur']; ?>"><?php echo $valuer['nom_acteur'].' '.$valuer['prenom_acteur']; ?></option>
     <?php
 }
 ?>
 </select>
 </div>
-<button onclick="plusAct()">Plus</button>
-<button>Ajouter Acteur</button>
+<p onclick="popact()">Créer Acteur</p>
 
 
 <!-- Créer un nouv choix de realisateur -->
 
 <p>Choix Realisateur</p>
 <div id="ChoixReal">
-<select name="Rea" id="">
+<select name="Rea">
+<?php
+foreach ($listR as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_real']; ?>"><?php echo $valuer['nom_real'].' '.$valuer['prenom_real']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Rea1">
+<option value="">Aucun</option>
+<?php
+foreach ($listR as $key => $valuer) {
+    ?>
+    <option value="<?php echo $valuer['id_real']; ?>"><?php echo $valuer['nom_real'].' '.$valuer['prenom_real']; ?></option>
+    <?php
+}
+?>
+</select>
+<select name="Rea2">
+<option value="">Aucun</option>
 <?php
 foreach ($listR as $key => $valuer) {
     ?>
@@ -67,48 +150,47 @@ foreach ($listR as $key => $valuer) {
 ?>
 </select>
 </div>
-<button onclick="plusReal()">Plus</button>
-<button>Ajouter Realisateur</button>
+<p onclick="popreal()">Créer Realisateur</p>
 
 
-<button type="submit" name="ajout">Ajouter</button>
+<button type="submit" name="ajout">Créer le film</button>
 
 <button type="submit" name="annul">Annuler</button>
 </form>
 <!-- Ajouter Acteur -->
-<form method="POST">
+<form method="POST" class="hide" id="creaAct">
 <input placeholder="nom" type="text" name="nom_act">
 <input placeholder="prenom" type="text" name="prenom_act">
 <input placeholder="age" type="number" name="age_act">
 <input placeholder="nationalite" type="text" name="nation_act">
 <input placeholder="photo" type="text" name="photo_act">
 
-<button type="submit" name="ajout_act">Ajouter</button>
+<button type="submit" name="ajout_act">Créer l'Acteur</button>
 
 <button type="submit" name="annul_act">Annuler</button>
 </form>
 <!-- ajout real -->
-<form method="POST">
+<form method="POST" class="hide" id="creaReal">
 <input placeholder="nom" type="text" name="nom_real">
 <input placeholder="prenom" type="text" name="prenom_real">
 <input placeholder="age" type="number" name="age_real">
 <input placeholder="nationalite" type="text" name="nation_real">
 <input placeholder="photo" type="text" name="photo_real">
 
-<button type="submit" name="ajout_real">Ajouter</button>
+<button type="submit" name="ajout_real">Créer le Réalisateur</button>
 
 <button type="submit" name="annul_real">Annuler</button>
 </form>
 
 <!-- ajout genre -->
-<form method="POST">
+<form method="POST" class="hide" id="creaGenre">
 <input placeholder="nom" type="text" name="nom_real">
 
 
-<button type="submit" name="ajout_genre">Ajouter</button>
+<button type="submit" name="ajout_genre">Créer le genre</button>
 
 <button type="submit" name="annul_genre">Annuler</button>
 </form>
-
+<p class="hide" id="closeAdm" onclick="closeAdm()">Fermer</p>
 <?php
 include 'footer.php';
